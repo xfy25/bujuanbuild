f46b7d740df64cca85c87d6378e39fe2cbe0420c
siu <xfy25@outlook.com>
2026-07-09 18:27:08 +0800

    update_compile_sdk

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.sixbugs.bujuan"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 34
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
